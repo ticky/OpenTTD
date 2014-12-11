6097624ed021efd2f087bdacd14ce27b02918e79
planetmaker <planetmaker@openttd.org>
2014-12-11 12:25:53 +0000

    Backport: (svn r27079) -Fix: Compilation with freetype2 version 2.5.4 and newer (AMDmi3)

--- src/fontcache.cpp
+++ src/fontcache.cpp
@@ -389,10 +389,6 @@ const Sprite *GetGlyph(FontSize size, WChar key)
 	GlyphEntry new_glyph;
 	GlyphEntry *glyph;
 	SpriteLoader::Sprite sprite;
-	int width;
-	int height;
-	int x;
-	int y;
 	int y_adj;
 
 	assert(IsPrintable(key));
@@ -419,8 +415,11 @@ const Sprite *GetGlyph(FontSize size, WChar key)
 	aa = (slot->bitmap.palette_mode == FT_PIXEL_MODE_GRAY);
 
 	/* Add 1 pixel for the shadow on the medium font. Our sprite must be at least 1x1 pixel */
-	width  = max(1, slot->bitmap.width + (size == FS_NORMAL));
-	height = max(1, slot->bitmap.rows  + (size == FS_NORMAL));
+	unsigned int width  = max(1U, (unsigned int)slot->bitmap.width + (size == FS_NORMAL));
+	unsigned int height = max(1U, (unsigned int)slot->bitmap.rows  + (size == FS_NORMAL));
+
+	/* Limit glyph size to prevent overflows later on. */
+	if (width > 256 || height > 256) usererror("Font glyph is too large");
 
 	/* FreeType has rendered the glyph, now we allocate a sprite and copy the image into it */
 	sprite.data = CallocT<SpriteLoader::CommonPixel>(width * height);
@@ -433,8 +432,8 @@ const Sprite *GetGlyph(FontSize size, WChar key)
 
 	/* Draw shadow for medium size */
 	if (size == FS_NORMAL) {
-		for (y = 0; y < slot->bitmap.rows; y++) {
-			for (x = 0; x < slot->bitmap.width; x++) {
+		for (unsigned int y = 0; y < (unsigned int)slot->bitmap.rows; y++) {
+			for (unsigned int x = 0; x < (unsigned int)slot->bitmap.width; x++) {
 				if (aa ? (slot->bitmap.buffer[x + y * slot->bitmap.pitch] > 0) : HasBit(slot->bitmap.buffer[(x / 8) + y * slot->bitmap.pitch], 7 - (x % 8))) {
 					sprite.data[1 + x + (1 + y) * sprite.width].m = SHADOW_COLOUR;
 					sprite.data[1 + x + (1 + y) * sprite.width].a = aa ? slot->bitmap.buffer[x + y * slot->bitmap.pitch] : 0xFF;
@@ -443,8 +442,8 @@ const Sprite *GetGlyph(FontSize size, WChar key)
 		}
 	}
 
-	for (y = 0; y < slot->bitmap.rows; y++) {
-		for (x = 0; x < slot->bitmap.width; x++) {
+	for (unsigned int y = 0; y < (unsigned int)slot->bitmap.rows; y++) {
+		for (unsigned int x = 0; x < (unsigned int)slot->bitmap.width; x++) {
 			if (aa ? (slot->bitmap.buffer[x + y * slot->bitmap.pitch] > 0) : HasBit(slot->bitmap.buffer[(x / 8) + y * slot->bitmap.pitch], 7 - (x % 8))) {
 				sprite.data[x + y * sprite.width].m = FACE_COLOUR;
 				sprite.data[x + y * sprite.width].a = aa ? slot->bitmap.buffer[x + y * slot->bitmap.pitch] : 0xFF;
