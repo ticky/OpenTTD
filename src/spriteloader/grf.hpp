JGRennison/OpenTTD-patches

(svn r14223) -Codechange: make GetSprite aware of the 4 different types of sprites: fonts, recolour, mapgen and normal sprites.

src/spriteloader/grf.hpp
@@ -12,7 +12,7 @@ public:
 	/**
 	 * Load a sprite from the disk and return a sprite struct which is the same for all loaders.
 	 */
-	bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, size_t file_pos);
+	bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, size_t file_pos, SpriteType sprite_type);
 };
 
 #endif /* SPRITELOADER_GRF_HPP */

src/spriteloader/spriteloader.hpp
@@ -26,7 +26,7 @@ public:
 	/**
 	 * Load a sprite from the disk and return a sprite struct which is the same for all loaders.
 	 */
-	virtual bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, size_t file_pos) = 0;
+	virtual bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, size_t file_pos, SpriteType sprite_type) = 0;
 
 	virtual ~SpriteLoader() { }
 };

ticky/OpenTTD

src/spriteloader/grf.hpp
@@ -12,7 +12,7 @@ public:
 	/**
 	 * Load a sprite from the disk and return a sprite struct which is the same for all loaders.
 	 */
-	bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, uint32 file_pos);
+	bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, uint32 file_pos, SpriteType sprite_type);
 };
 
 #endif /* SPRITELOADER_GRF_HPP */

src/spriteloader/spriteloader.hpp
@@ -26,7 +26,7 @@ public:
 	/**
 	 * Load a sprite from the disk and return a sprite struct which is the same for all loaders.
 	 */
-	virtual bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, uint32 file_pos) = 0;
+	virtual bool LoadSprite(SpriteLoader::Sprite *sprite, uint8 file_slot, uint32 file_pos, SpriteType sprite_type) = 0;
 
 	virtual ~SpriteLoader() { }
 };